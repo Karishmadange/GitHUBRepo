1b7d7bccb050e9b211c52014243e0fbcfb326ca7
Karishmadange <109783360+Karishmadange@users.noreply.github.com>
2023-06-03 18:23:32 +0530

    Update Additon.cpp
    changes done by karishma

--- Additon.cpp
+++ Additon.cpp
@@ -1,1 +1,14 @@
-
+class Addition
+{
+  int sum;
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the value of a & b");
+      int a=sc.nextInt();
+     int b=sc.nextInt();
+      sum=a+b;   
+    	System.out.println(sum);	
+      
+	}
+}
